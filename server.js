c0b245a8feb0a6de8a9f464d7a8f6fecdfd63b90
stelleal <stellalleal@gmail.com>
2022-08-02 16:30:22 -0300

    OAuth login /auth

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const https = require('https');
 const helmet = require('helmet');
 const express = require('express');
+const passport = require('passport');
+const { Strategy } = require('passport-google-oauth20');
 
 require('dotenv').config();
 
@@ -13,9 +15,24 @@ const config = {
   CLIENT_SECRET: process.env.CLIENT_SECRET,
 };
 
+const AUTH_OPTIONS = {
+  callbackURL: '/auth/google/callback',
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log('Google profile', profile);
+  done(null, profile);
+};
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback))
+
 const app = express();
 
 app.use(helmet());
+app.use(passport.initialize());
+
 
 function checkLoggedIn(req, res, next) {
   const isLoggedIn = true; //TODO
@@ -25,21 +42,38 @@ function checkLoggedIn(req, res, next) {
     });
   }
   next();
-}
+};
 
-app.get('/auth/google', (req, res) => { })
+app.get('/auth/google',
+  passport.authenticate('google', {
+    scope: ['email'],
+  })
+);
 
-app.get('/auth/google/callback', (req, res) => { })
+app.get('/auth/google/callback',
+  passport.authenticate('google', {
+    failureRedirect: '/failure',
+    successRedirect: '/',
+    session: false,
+  }),
+  (req, res) => {
+    console.log('Google called us back!');
+  }
+);
 
-app.get('/auth/logout', (req, res) => { })
+app.get('/auth/logout', (req, res) => { });
 
 app.get('/secret', checkLoggedIn, (req, res) => {
   return res.send('Your personal secret value is 24!');
-})
+});
+
+app.get('/failure', (req, res) => {
+  return res.send('Failed to log in!');
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-})
+});
 
 https.createServer({
   key: fs.readFileSync('key.pem'),

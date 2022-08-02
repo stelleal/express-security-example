373f07a599eca4b774f081accb1ffe3412ee316c
stelleal <stellalleal@gmail.com>
2022-08-02 14:57:11 -0300

    Dotenv CLIENT_ID and CLIENT_SECRET

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ const https = require('https');
 const helmet = require('helmet');
 const express = require('express');
 
+require('dotenv').config();
+
 const PORT = 3000;
+
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
 
 const app = express();
 
